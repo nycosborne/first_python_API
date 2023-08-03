fb387e6ac4865fb7aed4fc69ef2ac2851133dfc8
dano <nycosborne@gmail.com>
2023-08-03 13:57:03 -0400

    fun with API's

--- app.py
+++ app.py
@@ -28,6 +28,21 @@ def create_store():
     return new_store, 201
 
 
-# @app.post("/store/<string:name>")
-# def create_item(name):
-#     return name, 201
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store['items'].append(new_item)
+            return store, 201
+    return {"message": "Sorry no store by that name"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_store_items(name):
+    for store in stores:
+        if store['name'] == name:
+            return store['items'], 200
+
+    return {"message": "Sorry no store by that name"}, 404
